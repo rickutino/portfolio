68a8f285ca2e1cc0e74e3748905ba467283d312e
Ricardo Utino <rickutino@gmail.com>
2020-01-06 20:20:28 +0900

    to put it on one page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def about
-  end
-
-  def skill
-  end
-
-  def contact
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'home/index'
-  get '/about', to: 'home#about'
-  get '/skill', to: 'home#skill'
-  get '/contact', to: 'home#contact'
-  # get 'home/show'
-
-  resources :home , only:[:index,:show]
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
