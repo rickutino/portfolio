1c53c3e51c633bade9acaf35bfe4c6dda8a89463
Ricardo Utino <rickutino@gmail.com>
2020-02-10 01:47:30 +0900

    Change PWD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            'utinoricardo@gmail.com',
-    password:             '<%= ENV['DATABASE_PASSWORD'] %>',
+    password:             ENV['DATABASE_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
