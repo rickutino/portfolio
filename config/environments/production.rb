4e838152ed461bdcf4a10fb01c1d39801e949b42
Ricardo Utino <rickutino@gmail.com>
2020-02-10 01:28:38 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     port:                  587,
     domain:               'gmail.com',
     user_name:            'utinoricardo@gmail.com',
-    password:             'GH37e3ct1',
+    password:             '<%= ENV['DATABASE_PASSWORD'] %>',
     authentication:       'plain',
     enable_starttls_auto:  true
   }
