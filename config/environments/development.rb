ea29ffcd7633526d79bc0faa97b43a4d68bfa28e
Ricardo Utino <rickutino@gmail.com>
2020-02-09 16:04:49 +0900

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,17 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            'utinoricardo@gmail.com',
+    password:             'GH37e3ct1',
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
