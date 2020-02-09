0938e1d94654a7345b0e61ed627ff72044616670
Ricardo Utino <rickutino@gmail.com>
2020-02-09 16:06:56 +0900

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,58 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// function calcMedia(){
+//   return (this.nota[0]+this.nota[1])/2;
+// }
+
+// function mediaEscolar(){
+//     return (this.nota1+this.nota2)/2;
+// }
+
+// function criarAluno(nome,n1,n2){
+//   return{
+//     nome:nome,
+//     nota1:n1,
+//     nota2:n2,
+//     media:mediaEscolar
+//   }
+// }
+
+// function exame(nota){
+//   if ( nota >= 7){
+//     console.log("Voce passou!!");
+//   }else{
+//     console.log("Se esforce mais");
+//   }
+// }
+
+// var turma =[
+//   criarAluno("Ricardo", 9, 10),
+//   criarAluno("Gabriel", 10, 10),
+//   criarAluno("Maho", 4, 6.5)
+// ]
+
+
+
+// turma.forEach(function(element){
+//   console.log(`${element.nome} voce tirou ${element.media()} entao`)
+//   exame(element.media())
+// })
+
+// for(var i in turma){
+//   var aluno = turma[i]
+//   console.log(`${aluno.nome} voce tirou ${aluno.media()} entao`)
+//   exame(aluno.media())
+// }
+
+// var aluno = {
+//   nome:"ricardo",
+//   nota:[9.0,8.5],
+//   // media: calcMedia,
+//   media:function (){
+//     return (this.nota[0]+this.nota[1])/2;
+//   },
+// }
+// console.log(aluno.nome,aluno.media())
+// // console.log(aluno.media())
